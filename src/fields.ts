589e40e4ec18e4de5f46e0ca6b15938f0da83d4e
max.rodionov <rodionov.m.m@gmail.com>
2020-05-20 01:28:04 +0300

    feat: add optionalField(), sequencesField() field factory helpers

--- src/fields.ts
+++ src/fields.ts
@@ -5,3 +5,12 @@ export const indexField = (): FieldSimpleFactory<number> => (ctx) => ctx.index;
 export const nullableField = <T>(initialValue: T | null): T | null => {
     return initialValue;
 };
+
+export const optionalField = <T>(initialValue: T | undefined): T | undefined => {
+    return initialValue;
+};
+
+export const sequenceField = <T>(items: T[]): FieldSimpleFactory<T> => {
+    const total = items.length;
+    return (ctx) => items[(ctx.index - 1) % total];
+};
